bccf845b852f1b1f1119821736fe5caac87224ff
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-25 10:57:28 +0200

    Add functionality: showing details of selected country

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,15 @@ import Home from './presentational/home.component';
 import Contact from './presentational/contact.component';
 import NotFound from './presentational/not-found.component';
 import CountryFlagContainer from './containers/flag-container.component';
+import CountryDetailsContainer from './containers/country-details-container';
 
 export default (
     <Route path='/' component={Navigation}>
       <IndexRoute component={Home}/>
-      <Route path='countries' component={CountryFlagContainer}/>
-      <Route path='countries/country/:id' component={CountryDetailsContainer}/>
+      <Route path='countries'>
+        <IndexRoute component={CountryFlagContainer}/>
+        <Route path='country/:id' component={CountryDetailsContainer}/>
+      </Route>
       <Route path='contact' component={Contact}/>
       <Route path='*' component={NotFound}/>
     </Route>
